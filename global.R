25b1ae7ea1fb4134557f65af342ac8f366569983
Yunong <33441530+yunong-xia@users.noreply.github.com>
2020-08-18 11:20:42 -0500

    Update global.R

--- global.R
+++ global.R
@@ -1,5 +1,7 @@
 #Library we will use
   ##Library for rendering
+options(rgl.useNULL = TRUE)
+
 library(shiny)
 library(shinydashboard)
 library(shinyhelper)
@@ -21,15 +23,13 @@ source("dataset_import.R")
 source("two_drug.R")
 source("two_drug_batch.R")
 source("control_plus_one.R")
-source("helper_func.R")
 source("control_plus_one_batch.R")
 source("test_vs_control.R")
 source("test_vs_control_batch.R")
 
 
 
-#options
-options(rgl.useNULL = TRUE)
+
 options(shiny.maxRequestSize = 300*1024^2)
 par3d(cex = 1)
 options(stringsAsFactors = FALSE)
